08c583868deb6b64125ac6a8c2dbee997d008bf2
fabiwoller <fabianwoller@web.de>
2026-01-15 14:09:47 +0100

    Add version to compiled module.

--- python/napypi/__init__.py
+++ python/napypi/__init__.py
@@ -1,5 +1,6 @@
 from .wrapper import pearsonr, spearmanr, chi_squared, anova
 from .wrapper import kruskal_wallis, ttest, mwu
+from importlib.metadata import version, PackageNotFoundError
 
 __all__ = ["pearsonr",
            "spearmanr",
@@ -8,3 +9,8 @@ __all__ = ["pearsonr",
            "kruskal_wallis",
            "ttest",
            "mwu"]
+
+try:
+    __version__ = version("napypi")
+except PackageNotFoundError:
+    __version__ = "unknown"
